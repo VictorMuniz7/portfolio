86e54d28d08d80f5228acccf2846c71b979823ec
Kairuus <kingkairuus@gmail.com>
2024-02-24 12:12:49 -0300

    button fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-R5WS5TS5.mjs";import"./chunk-HERLOIB3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-XPG2BXGK.mjs";import"./chunk-HERLOIB3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
